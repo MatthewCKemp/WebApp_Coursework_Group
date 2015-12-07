b9ceaa6910a270d92f44c447ff3f3353f0f23969
1304963 <MatthewCKemp>
2015-12-07 14:56:16 +0000

    Privileges_Populate_v.1.1

---
+++ Privileges_Populate.sql
@@ -0,0 +1,34 @@
+<?php
+$conn = new mysqli(
+
+    "eu-cdbr-azure-west-c.cloudapp.net"
+    "befd2db9fe76ad"
+    "5a698004"
+    "WebApp_Coursework"
+);
+
+/*Establishes connection*/
+
+//$conn - newmysqli($servername, $username, $password, $dbname);
+/*Connection check*/
+if ($conn->connect_error) {
+  die("Connection has failed " . $conn->connect_error);
+}
+/* sql to create table*/
+$sql = "
+INSERT INTO markers (privID, description) VALUES ('PR01', 'Basic User');
+INSERT INTO markers (privID, description) VALUES ('PR02', 'Reader');
+INSERT INTO markers (privID, description) VALUES ('PR03', 'Author');
+INSERT INTO markers (privID, description) VALUES ('PR04', 'Admin');
+INSERT INTO markers (privID, description) VALUES ('PR05', 'Admin and reader');
+INSERT INTO markers (privID, description) VALUES ('PR06', 'Admin and author');
+";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table markers created";
+} else {
+  echo "Error found creating table " . $conn->error;
+}
+
+$conn->close();
+?>
