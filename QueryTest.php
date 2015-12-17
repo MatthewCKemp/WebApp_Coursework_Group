1d8e6397c6d366afad0b40d1f195bd8e315cb328
1304963 <MatthewCKemp>
2015-12-17 14:54:07 +0000

    QTest

--- QueryTest.php
+++ QueryTest.php
@@ -18,12 +18,6 @@ $result = $db->query($sql_query);
 // iterate over $result object one $row at a time
 // use fetch_array() to return an associative array
 while($row = $result->fetch_array()){
-// process the $row
-}
-
-// iterate over $result object one $row at a time
-// use fetch_array() to return an associative array
-while($row = $result->fetch_array()){
     // print out fields from row of data
     echo "<p>” . $row ['adventID'] . “</p>";
 }
