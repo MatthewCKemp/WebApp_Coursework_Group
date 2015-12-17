ce383f56e727932bca88d3f9271e3aef48578cb3
1304963 <MatthewCKemp>
2015-12-17 15:43:42 +0000

    QTest23

--- QueryTest.php
+++ QueryTest.php
@@ -19,7 +19,7 @@ $result = $db->query($sql_query);
 // use fetch_array() to return an associative array
 while($row = $result->fetch_array()){
     // print out fields from row of data
-    echo "<p>” . $row advent_name . “</p>";
+    echo $row . advent_name;
 }
 
 $result->close();
