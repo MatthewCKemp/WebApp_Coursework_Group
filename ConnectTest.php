88983386cafd59df1bfb61c206ecf0038ad3169b
1304963 <MatthewCKemp>
2015-12-17 13:04:34 +0000

    ConnectTest

---
+++ ConnectTest.php
@@ -0,0 +1,11 @@
+<?php
+$db = new mysqli(
+    "eu-cdbr-azure-west-c.cloudapp.net",
+    "befd2db9fe76ad",
+    "5a698004",
+    "WebApp_Coursework" );
+
+// test our connection
+if ($db->connect_errno) {
+    die ('Connection Failed :'.$db->connect_error );
+}
