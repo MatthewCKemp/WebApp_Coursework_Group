174692cf6026e687845b47923fa1b61e6c8f5383
1304963 <MatthewCKemp>
2015-12-17 13:38:00 +0000

    Query

--- QueryTest.php
+++ QueryTest.php
@@ -1,5 +1,21 @@
 <?php
 
+$db = new mysqli(
+    "eu-cdbr-azure-west-c.cloudapp.net",
+    "befd2db9fe76ad",
+    "5a698004",
+    "WebApp_Coursework" );
+
+// test our connection
+if ($db->connect_errno) {
+    die ('Connection Failed :'.$db->connect_error );
+}
+
+$conn = new mysqli(
+    "eu-cdbr-azure-west-c.cloudapp.net",
+    "befd2db9fe76ad",
+    "5a698004",
+    "WebApp_Coursework" );
 
 
 $query = "SELECT * FROM adventure WHERE 1";
@@ -7,3 +23,8 @@ $result = $db->query($query);
 if (!$result) {
     die('Nothing in result: ');
 }
+
+$result->close();
+$db->close();
+
+?>
