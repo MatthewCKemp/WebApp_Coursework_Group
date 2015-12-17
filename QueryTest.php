691b135759b37a633876ad7ac84b06b897e679ba
1304963 <MatthewCKemp>
2015-12-17 14:27:29 +0000

    QTest

--- QueryTest.php
+++ QueryTest.php
@@ -1,32 +1,29 @@
 <?php
-
+// connect to your Azure server and select database (remember you connection details are all on the azure portal
 $db = new mysqli(
     "eu-cdbr-azure-west-c.cloudapp.net",
     "befd2db9fe76ad",
     "5a698004",
-    "WebApp_Coursework" );
-
-// test our connection
-if ($db->connect_errno) {
-    die ('Connection Failed :'.$db->connect_error );
+    "WebApp_Coursework"
+);
+// test if connection was established, and print any errors
+if($db->connect_errno){
+    die("Connection failed: " . $conn->connect_error);
 }
 
-$conn = new mysqli(
-    "eu-cdbr-azure-west-c.cloudapp.net",
-    "befd2db9fe76ad",
-    "5a698004",
-    "WebApp_Coursework" );
+$sql_query = "SELECT * FROM adventure WHERE adventID LIKE '%0%'";
+// execute the SQL query
+$result = $db->query($sql_query);
 
-
-$queryAdventure = "SELECT * FROM adventure ";
-$resultAdventure = $db->query($queryAdventure);
-if (!$resultAdventure) {
-    die('Nothing in result: ');
+// iterate over $result object one $row at a time
+// use fetch_array() to return an associative array
+while($row = $result->fetch_array()){
+// process the $row
 }
 
-echo $resultAdventure;
-
-$result->close();
-$db->close();
-
-?>
+// iterate over $result object one $row at a time
+// use fetch_array() to return an associative array
+while($row = $result->fetch_array()){
+    // print out fields from row of data
+    echo "<p>” . $row 'adventID' . “</p>";
+}
