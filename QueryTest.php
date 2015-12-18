60bc281fc18c6a892bbeb4ab2e7bd811eeb911f5
tsvetimarinova <tsvetoslavka@abv.bg>
2015-12-18 00:50:34 +0000

    Update QueryTest.php

--- QueryTest.php
+++ QueryTest.php
@@ -1,16 +1,28 @@
 <?php
 // connect to your Azure server and select database (remember you connection details are all on the azure portal
-$db = new mysqli(
-    "eu-cdbr-azure-west-c.cloudapp.net",
-    "befd2db9fe76ad",
-    "5a698004",
-    "WebApp_Coursework"
-);
+//$db = new mysqli(
+   // "eu-cdbr-azure-west-c.cloudapp.net",
+    //"befd2db9fe76ad",
+   // "5a698004",
+   // "WebApp_Coursework"
+//);
 // test if connection was established, and print any errors
-if($db->connect_errno){
-    die("Connection failed: " . $conn->connect_error);
-}
-mysql_select_db('WebApp_Coursework')
+//if($db->connect_errno){
+//    die("Connection failed: " . $conn->connect_error);
+//}
+
+
+
+$db_database = 'WebApp_Coursework';
+$db_hostname = 'eu-cdbr-azure-west-c.cloudapp.net';
+$db_username = 'befd2db9fe76ad';
+$db_password = '5a698004';
+
+
+$db_server = mysql_connect($db_hostname, $db_username, $db_password);
+if(!$db_server) die("Unable to connect to MYSQL: ". mysql_error());
+
+mysql_select_db('$db_database')
     or die("Unable to connect to database: " . mysql_error());
     
 $sql_query = "SELECT * FROM adventure";
